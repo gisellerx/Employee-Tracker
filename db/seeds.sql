08d53002307f8495d8633fbf27bfd633d5c0b8c4
gisellerx <giselle.armas@outlook.com>
2023-10-05 21:22:59 -0400

    added seeds for employees with first and last name and their role id

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,3 +16,13 @@ VALUES ("Sales Lead", 100000, 1),
        ("Legal Team Lead", 250000, 4),
        ("Lawyer", 190000, 4);
 
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Doe", 1),
+       ("Mike", "Chan", 2),
+       ("Ashley", "Rodriguez", 3)
+       ("Kevin", "Tupik", 4),
+       ("Kunal", "Singh", 5),
+       ("Malia", "Brown", 6),
+       ("Sarah", "Lourd", 7),
+       ("Tom", "Allen", 8);
+

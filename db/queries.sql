21f7b611e92e366d885dc73bfdaf687dec9b099a
gisellerx <giselle.armas@outlook.com>
2023-10-05 22:35:55 -0400

    added query for employee table to show roles, department, and managers

--- db/queries.sql
+++ db/queries.sql
@@ -3,4 +3,11 @@ USE employees_db;
 SELECT * FROM department;
 
 SELECT role.id , title, salary, name as department 
-from role LEFT JOIN department on department.id = role.department_id
+from role 
+LEFT JOIN department on department.id = role.department_id
+
+SELECT employee.id , first_name, last_name, title, salary, name as department, salary, CONCAT(bosses.first_name, ' ',bosses.last_name) as manager 
+from employee 
+LEFT JOIN role on employee.role_id.role.id
+LEFT JOIN department on department.id = role.department_id
+LEFT JOIN employee as bosses on employee.manager_id = bosses.id;

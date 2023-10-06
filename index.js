4ad6e6b4327e2a8476f7f7ef08a92e5fff5c5940
gisellerx <giselle.armas@outlook.com>
2023-10-05 23:39:06 -0400

    added main menu function

--- index.js
+++ index.js
@@ -11,3 +11,72 @@ const db = mysql.createConnection({
     port: 3306
 })
 
+db.connect(() => {
+    mainMenu()
+})
+
+function mainMenu() {
+    inquirer
+      .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ]
+        })
+        .then(answer => {
+
+            if (answer.action === 'View all departments') {
+                viewDepartments()
+            } else if (answer.action === 'View all roles') {
+                viewRoles()
+            } else if (answer.action === 'View all employees') {
+                viewEmployees()
+            } else if (answer.action === 'Add a department') {
+                addDepartment()
+            } else if (answer.action === 'Add a role') {
+                addRole()
+            } else if (answer.action === 'Add an employee') {
+                addEmployee()
+            } else if (answer.action === 'Update an employee role') {
+                updateEmployeeRole()
+            }
+        })
+}
+
+
+function viewDepartments() {
+
+}
+
+function viewRoles() {
+
+}
+
+function viewEmployees() {
+
+}
+
+
+function addDepartment() {
+
+}
+
+function addRole() {
+
+}
+
+function addEmployee() {
+
+}
+
+function updateEmployeeRole() {
+
+}

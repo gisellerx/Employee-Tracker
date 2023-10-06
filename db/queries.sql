5365f3d256b83f8715015230b71ec3a47b7820f3
gisellerx <giselle.armas@outlook.com>
2023-10-05 23:18:37 -0400

    made index.js and downloaded inquirer, mysql2, and console-table-printer

--- db/queries.sql
+++ db/queries.sql
@@ -6,8 +6,7 @@ SELECT role.id , title, salary, name as department
 from role 
 LEFT JOIN department on department.id = role.department_id
 
-SELECT employee.id , first_name, last_name, title, salary, name as department, salary, CONCAT(bosses.first_name, ' ',bosses.last_name) as manager 
-from employee 
-LEFT JOIN role on employee.role_id.role.id
+SELECT employee.id, employee.first_name, employee.last_name, title, name as department ,salary, CONCAT(bosses.first_name, ' ',bosses.last_name) as manager from employee
+LEFT JOIN role on employee.role_id = role.id
 LEFT JOIN department on department.id = role.department_id
 LEFT JOIN employee as bosses on employee.manager_id = bosses.id;

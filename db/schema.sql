b7429191b7f30dbe2321ac3d9df87e3395b7cb7b
gisellerx <giselle.armas@outlook.com>
2023-10-05 22:12:38 -0400

    found error in schema on role_id and manager_id where comma needs to be separate from INT

--- db/schema.sql
+++ db/schema.sql
@@ -11,6 +11,7 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
@@ -21,12 +22,12 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-
+    -- for some reason, these commas below need to be separate from INT
+    role_id INT ,
+    manager_id INT ,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL
+    ON DELETE SET NULL,
 
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -6,7 +6,7 @@ VALUES ("Sales"),
        ("Finance"),
        ("Legal");
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title,salary,department_id)
 VALUES ("Sales Lead", 100000, 1),
        ("Salesperson", 80000, 1),
        ("Lead Engineer", 150000, 2),
@@ -19,10 +19,15 @@ VALUES ("Sales Lead", 100000, 1),
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("John", "Doe", 1),
        ("Mike", "Chan", 2),
-       ("Ashley", "Rodriguez", 3)
+       ("Ashley", "Rodriguez", 3),
        ("Kevin", "Tupik", 4),
        ("Kunal", "Singh", 5),
        ("Malia", "Brown", 6),
        ("Sarah", "Lourd", 7),
        ("Tom", "Allen", 8);
+
+UPDATE employee SET manager_id = 1 where id = 2;
+UPDATE employee SET manager_id = 3 where id = 4;
+UPDATE employee SET manager_id = 5 where id = 6;
+UPDATE employee SET manager_id = 7 where id = 8;
 
